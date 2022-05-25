65a503e93b8818329847a67718c35ad1e4bf26ae
jiyeong-seo <92812508+jiyeong-seo@users.noreply.github.com>
2022-05-25 17:12:45 +0900

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,38 @@
+import "./styles.css";
+import { useState } from "react";
+
+export default function App() {
+  let [titles, setTitle] = useState([
+    "남자코트 추천",
+    "강남 우동맛집",
+    "파이썬 독학",
+    "자바스크립트 독학"
+  ]);
+  let likeList = titles.map((title) => 0);
+  let [likes, setLike] = useState(likeList);
+
+  return (
+    <div className="App">
+      {titles.map((title, index) => {
+        return (
+          <div className="list" key={index}>
+            <h4>
+              {titles[index]}
+              <span
+                onClick={() => {
+                  let copyLikes = [...likes];
+                  copyLikes[index] = copyLikes[index] + 1;
+                  setLike(copyLikes);
+                }}
+              >
+                like
+              </span>
+              {likes[index]}
+            </h4>
+            <p>5월 25일 발행</p>
+          </div>
+        );
+      })}
+    </div>
+  );
+}
